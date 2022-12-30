c6f570574312a5b3725e156d105f9c2fb6534ad3
Slim-Bob <112772549+Slim-Bob@users.noreply.github.com>
2022-12-30 23:31:04 +0500

    SF_D7 Реализация переодической отправки сообщений по триггеру

--- NewsPaper/NewsPaper/celery.py
+++ NewsPaper/NewsPaper/celery.py
@@ -1,9 +1,19 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
 
 app = Celery('NewsPaper')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.beat_schedule = {
+    'periodic_mailing_every_on_monday': {
+        'task': 'news.tasks.periodic_mailing',
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+        # 'schedule': 10.0, #для теста
+        # 'args': (5, 5),
+    },
+}
 
 app.autodiscover_tasks()
--- NewsPaper/news/tasks.py
+++ NewsPaper/news/tasks.py
@@ -2,7 +2,8 @@ from celery import shared_task
 from django.template.loader import render_to_string
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
-
+from .models import Post, Category
+import datetime
 
 @shared_task
 def test():
@@ -34,6 +35,42 @@ def send_notifications(preview, pk, title, subscribers):
             from_email=settings.DEFAULT_FROM_EMAIL,
             to=subscribers
         )
+
+    msg.attach_alternative(html_context, 'text/html')
+    msg.send()
+
+
+@shared_task
+def periodic_mailing():
+    today = datetime.datetime.now()
+    last_week = today - datetime.timedelta(days=7)
+    posts = Post.objects.filter(create_date_time__gte=last_week)
+    categories = set(posts.values_list('category__name', flat=True))
+    emails = set(Category.objects.filter(name__in=categories).values_list('subcribers__email', flat=True))
+
+    for email in emails:
+        if email:
+            send_daily_posts(posts, [email, ])
+            print('end send email')
+
+
+def send_daily_posts(posts, subscribers):
+    print(subscribers)
+
+    html_context = render_to_string(
+        'daily_posts.html',
+        {
+            'posts': posts,
+            'link': settings.SITE_URL
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject='Статьи за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers
+    )
 
     msg.attach_alternative(html_context, 'text/html')
     msg.send()
