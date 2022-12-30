f5d1419da77cd936892b5ead1f73b2ba280dc910
Slim-Bob <112772549+Slim-Bob@users.noreply.github.com>
2022-12-30 23:14:15 +0500

    SF_D7 Реализация оповещения о добавлении статить через задачу

--- NewsPaper/news/signals.py
+++ NewsPaper/news/signals.py
@@ -6,6 +6,7 @@ from django.template.loader import render_to_string
 from django.conf import settings
 from .models import PostCategory
 
+from .tasks import test, send_notifications_category
 
 def send_notifications(preview, pk, title, subscribers):
     print(subscribers)
@@ -31,11 +32,19 @@ def send_notifications(preview, pk, title, subscribers):
 
 @receiver(m2m_changed, sender=PostCategory)
 def notify_about_new_post(sender, instance, **kwargs):
+    print('Start send email')
     if kwargs['action'] == 'post_add':
         categories = instance.category.all()
+        emails = list(categories.values_list('subcribers__email', flat=True))
 
-        for category in categories:
-            for sub in category.subcribers.all():
-                send_notifications(instance.preview(), instance.pk, instance.title, [sub.email, ])
-                print('end send email')
+        # test.delay()
+        # Можно просто передать post.pk, но была рекомендация что бы в задачу передавались только данные.
+        # Мне кажется это не совсем верным, ошибаюсь?
+        send_notifications_category.delay(emails=emails, preview=instance.preview(), pk=instance.pk, title=instance.title)
+
+        # Вынесено в задачу
+        # for category in categories:
+        #     for sub in category.subcribers.all():
+        #         send_notifications(instance.preview(), instance.pk, instance.title, [sub.email, ])
+        #         print('end send email')
 
--- NewsPaper/news/tasks.py
+++ NewsPaper/news/tasks.py
@@ -1,6 +1,39 @@
 from celery import shared_task
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
 
 
 @shared_task
 def test():
     print('Test')
+
+
+@shared_task
+def send_notifications_category(emails, preview, pk, title):
+    print('start send email in task')
+    for email in emails:
+        send_notifications(preview, pk, title, [email, ])
+    print('end send email in task')
+
+
+def send_notifications(preview, pk, title, subscribers):
+    print(subscribers)
+
+    html_context = render_to_string(
+        'post_created_email.html',
+        {
+            'text': preview,
+            'link': f'{ settings.SITE_URL }/news/post/{ pk }'
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+            subject=title,
+            body='',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to=subscribers
+        )
+
+    msg.attach_alternative(html_context, 'text/html')
+    msg.send()
